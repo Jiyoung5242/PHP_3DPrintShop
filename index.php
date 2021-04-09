769f0a7c06ffb598c9135eecf8ba7771205ad4aa
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 10:25:18 -0400

    chagne to index to allow login and other pages

--- index.php
+++ index.php
@@ -29,7 +29,33 @@ Licence URI: https://www.os-templates.com/template-terms
 
 
 <div class="bgded overlay" style="background-image:url('/php_3dprintshop/images/backgrounds/main_image.jpg');"> 
-<?php include "./pages/topbar.php" ?> 
+<div class="wrapper row0">
+    <div id="topbar" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="fl_left"> 
+        <!-- ################################################################################################ -->
+        <ul class="nospace">
+          <li><i class="fas fa-phone"></i> +1 226 743 0596</li>
+          <li><i class="far fa-envelope"></i> 3dprintshop@gmail.com</li>
+        </ul>
+        <!-- ################################################################################################ -->
+      </div>
+      <div class="fl_right"> 
+        <!-- ################################################################################################ -->
+        <ul class="nospace">
+          <li><div id="welcome" style="<?php if(isset($_SESSION['email']) && $_SESSION['email'] != "") { ?> display:block; <?php } ?> ">
+          <?php  if(isset($_SESSION['email'])) {echo $_SESSION['email']; } ?></div></li>
+          <li><a href="./index.php"><i class="fas fa-home"></i></a></li>
+          <li><a href="./pages/logout.php" title="Logout"><i class="fas fa-sign-out-alt"></i></a></li>
+          <li><a href="./pages/login.php" title="Login"><i class="fas fa-sign-in-alt"></i></a></li>
+          <li><a href="./pages/register.php" title="Sign Up"><i class="fas fa-edit"></i></a></li>
+        </ul>
+        <!-- ################################################################################################ -->
+      </div>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+
  <div class="wrapper row1">
     <header id="header" class="hoc clear"> 
       <div id="logo" class="fl_left">
